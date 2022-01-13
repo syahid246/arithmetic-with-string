d63d6b1aa1b8b5b33c389d760c8d56c151647d4b
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2022-01-14 00:47:26 +0700

    We can print in Vanilla Javascript (Not Supported in most Browser)
    We can print in Vanilla Javascript with BigInt.
    But, BigInt is not supported in most browsers. See in https://caniuse.com/?search=BigInt

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,2 +1,3 @@
 console.log('9999999999999999') // It's print 9999999999999999
+console.log(9999999999999999n) // It's print 9999999999999999, but BigInt is not supported in most browsers (https://caniuse.com/?search=BigInt)
 console.log(9999999999999999) // It's print 10000000000000000
