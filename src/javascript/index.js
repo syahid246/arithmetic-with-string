d581e02599537f53bdd107793ee23b7e515e7cab
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2022-01-15 00:24:40 +0700

    Check "greater than" from the first number then proceed to the next number
    Check "greater than" from the first number then proceed to the next number and so on. If the number greater than, return true.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -36,6 +36,31 @@ console.log(isNumber(9999999999999999)) // false
 function isGreater(a, b) {
     if ( isNumber(a) && isNumber(b) ) {
         if ( a.length > b.length ) return true
+        if ( a.length === b.length ) {
+            // Object for convert string to number
+            const numbers = {
+              '0': 0,
+              '1': 1,
+              '2': 2,
+              '3': 3,
+              '4': 4,
+              '5': 5,
+              '6': 6,
+              '7': 7,
+              '8': 8,
+              '9': 9,
+            }
+            let numA = 0,
+                numB = 0
+
+            // check the "greater than" of the first number then move on to the second number and so on (from the front)
+            for (let i = 0; i < a.length; i++) {
+                // Convert string to number by accessing {numbers} object
+                numA = numbers[a.substr(i, 1)]
+                numB = numbers[b.substr(i, 1)]
+                if ( numA > numB ) return true
+            }
+        }
     }
     return false
 }
@@ -44,4 +69,4 @@ console.log(isGreater('12', '1') === true) // true
 console.log(isGreater('1', '1') === false) // true
 console.log(isGreater('2', '2') === false) // true
 console.log(isGreater('1', '2') === false) // true
-console.log(isGreater('2', '1') === true) // false
+console.log(isGreater('2', '1') === true) // true
